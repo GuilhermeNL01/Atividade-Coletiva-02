61e23c7f22132a80983333c5fe46976d554734dc
Fiboliriodem <felipe123654_vcardoso@outlook.com>
2024-06-15 14:20:39 -0300

    metodo principal

--- SocketLibrary/src/main/java/cliente/Cliente.java
+++ SocketLibrary/src/main/java/cliente/Cliente.java
@@ -17,5 +17,51 @@ public class Cliente {
     private static final String CADASTRAR_LIVRO = "4";
     private static final String SAIR = "5";
 
+    // principal
+    public static void main(String[] args) {
+        try {
+            //inicializando socket
+            Socket socket = new Socket(HOST, PORTA);
+
+           // Leitores pro sistema
+
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter saida = new PrintWriter(socket.getOutputStream(), true);
+            Scanner scanner = new Scanner(System.in);
+
+            //use cases
+            String opcao;
+            do {
+                exibirMenu();
+                opcao = scanner.nextLine();
+
+                switch (opcao) {
+                    case LISTAR_LIVROS:
+                        listarLivros(saida, entrada);
+                        break;
+                    case ALUGAR_LIVRO:
+                        alugarLivro(saida, entrada, scanner);
+                        break;
+                    case DEVOLVER_LIVRO:
+                        devolverLivro(saida, entrada, scanner);
+                        break;
+                    case CADASTRAR_LIVRO:
+                        cadastrarLivro(saida, entrada, scanner);
+                        break;
+                    case SAIR:
+                        saida.println("sair");
+                        System.out.println("Saindo...");
+                        break;
+                    default:
+                        System.out.println("Opção inválida.");
+                }
+            } while (!opcao.equals(SAIR));
+
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
