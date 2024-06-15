4742f25beb2f4bfde3421413f0fab5b51abd6e26
Fiboliriodem <felipe123654_vcardoso@outlook.com>
2024-06-15 14:17:51 -0300

    portas e opçoes

--- SocketLibrary/src/main/java/cliente/Cliente.java
+++ SocketLibrary/src/main/java/cliente/Cliente.java
@@ -1,4 +1,21 @@
 package cliente;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Cliente {
+
+    // portas
+    private static final String HOST = "localhost";
+    private static final int PORTA = 12344;
+
+    //opcoes
+    private static final String LISTAR_LIVROS = "1";
+    private static final String ALUGAR_LIVRO = "2";
+    private static final String DEVOLVER_LIVRO = "3";
+    private static final String CADASTRAR_LIVRO = "4";
+    private static final String SAIR = "5";
+
+
 }
